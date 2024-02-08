3a4884641b6ff3a2e6d6c798ff82d3051aa3e24d
developervinay1 <developervsandhu@gmail.com>
2024-02-08 15:25:55 -0500

    Basic Relation of Models

--- models/todos/sub_todo.model.js
+++ models/todos/sub_todo.model.js
@@ -15,4 +15,4 @@ const subTodo_Schema = new mongoose.Schema({
     }
 }, {timestamps: true});
 
-export const SubTodo = mongoose.model("SubTodo", subTodo_Schema)
+export const SubTodo = mongoose.model("SubTodo", subTodo_Schema);
